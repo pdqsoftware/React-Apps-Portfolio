668537833a4c19f45b31f0c6d061d9b8031f8664
Paul Pritchard <paulp@computerspecialists.co.za>
2020-08-24 13:47:49 +0200

    Boilerplate code for React Apps created

---
+++ boilerplate/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+// JSX - JavaScript XML
+var template = <p>Boilerplate JSX from app.js!</p>
+var appRoot = document.getElementById('app')
+ReactDOM.render(template, appRoot)

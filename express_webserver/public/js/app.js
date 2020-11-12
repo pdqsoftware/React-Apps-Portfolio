17d337e4bb2771077e26cae5a37c3e1629b793b3
Paul Pritchard <paulp@computerspecialists.co.za>
2020-11-12 17:11:30 +0200

    express_webserver: Tidied up with some small changes and new CSS

--- express_webserver/public/js/app.js
+++ express_webserver/public/js/app.js
@@ -13,6 +13,8 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -22,21 +24,28 @@ weatherForm.addEventListener('submit', (e) => {
     console.log(location)
 
     if (location) {
+        messageOne.textContent = 'Fetching...'
+        messageTwo.textContent = ''
+
         fetch('http://localhost:3000/weather?location=' + location)
         .then((response) => {
             response.json()
             .then((data) => {
                 if (data.error) {
-                    console.log(`Error: ${data.error}`)
+                    // console.log(`Error: ${data.error}`)
+                    messageOne.textContent = data.error
                 } else {
-                    console.log(data.location)
-                    console.log(data.forecast)
+                    // console.log(data.location)
+                    // console.log(data.forecast)
+                    messageOne.textContent = data.location 
+                    messageTwo.textContent = data.forecast
                 }
             })
         })
 
     } else {
         console.log('You must enter a location!')
+        messageOne.textContent = 'You must enter a location!'
     }
 
     
